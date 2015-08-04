b2cc35331fb234ed9a34f5b66ddbda53e4bc35fa
Dacheng Jiang <dacheng.jiang@xjtlu.edu.cn>
2015-08-04 15:28:39 +0800

    second

--- main.c
+++ main.c
@@ -53,9 +53,40 @@ int string_is_hex(char *c) {
 }
 
 int main() {
+    unsigned long i,a,b;
     char s[255];
     char *c;
-    FILE *fp;
-    fp = fopen("../kern.log", "r");
+    FILE *fp, *fout;
+    fp = fopen("firefox_1.log", "r");
     fout = fopen("temp.txt", "w");
+    long unsigned int addr_start, addr_length;
+    int frame_count = 1;
+    scanf("%lx %lu", &addr_start, &addr_length);
+    while (fgets(s, 255, fp) != NULL) {
+        c = s;
+        while (*c != ']') c++;
+        c++;
+        //if (string_is_Y(c)) continue;
+        //if (string_is_N(c)) continue;
+        //if (string_is_sam(c)) continue;
+        //if (string_is_That(c)) continue;
+        //if (string_is_other(c)) continue;
+        if (string_is_quit(c)) break;
+        if (string_is_That(c)) {
+            //fprintf(fout, "%d\n", 0);
+            frame_count++;
+            continue;
+        }
+        if (!(string_is_hex(c))) continue;
+        sscanf(c, "%lX - %lX", &a, &b);
+        if ((a >= addr_start) && (a < (addr_start + addr_length * 1024))) {
+            for (; a<=b; a+=4096) {
+                fprintf(fout, "%d %lX\n", frame_count, a);
+            }
+        }
+//        if ((a > addr_start))
+//        printf("%lX - %lX\n", a, b);
+
+//        do_with(a, b);
+    }
 }
